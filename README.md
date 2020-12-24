9267d84924a7584019bf6271b38ea972e3760424
Henok Addis <headdis@microsoft.com>
2020-12-24 01:26:18 -0500

    enable checking schedule

---
+++ README.md
@@ -0,0 +1,63 @@
+"# espnfantasybball" 
+
+##Example API query
+https://fantasy.espn.com/apis/v3/games/fba/seasons/2021/segments/0/leagues/<<INSERTLEAGUEID>>?view=mLiveScoring&view=mMatchupScore&view=mPendingTransactions&view=mPositionalRatings&view=mRoster&view=mSettings&view=mTeam&view=modular&view=mNav
+
+##Documenting API responses
+###Average Stats
+0 = points
+1 = blocks
+2 = steals
+3 = assist
+6 = rebounds
+11 = Turnovers
+13 = fgm
+14 = fga
+15 = ftm
+16 = fta
+17 = 3pm
+19 = fg%
+20 = ft%
+
+###schedule
+	away
+		rosterForCurrentScoringPeriod
+		rosterForMatchupPeriodDelayed
+		teamId
+		tiebreak
+		totalPoints
+	home
+		rosterForCurrentScoringPeriod 
+		rosterForMatchupPeriodDelayed
+		teamId
+		tiebreak
+		totalPoints
+	id
+	matchupPeriodId
+	playoffTierType
+	winner
+
+###teams
+	"id": int,
+	"nickname": string
+	"draftDayProjectedRank": int
+	"roster": object
+		"entries": array of player objects
+			"playerPoolEntry": object
+				"player": object
+					"eligibleSlots": array of ints (places player can fit)
+					"fullName": string
+					"id": int
+					"injured": binary
+                                	"injuryStatus": string ("ACTIVE")
+					"proTeamId": int (team name)
+					"stats": array of objects
+						0 - 2021 season
+							"averageStats": object (see averageStats breakdown)
+						1 - 2021 season (??)
+						2 - 2020 season
+						3 - 2021 season (??)
+						4 - 2021 projections					
+			
+			
+
---
+++ fantasyH2HCombos.py
@@ -0,0 +1,63 @@
+import json
+import datetime
+from datetime import datetime
+#League starts week 52 of 2020
+with open(r"C:\Users\headdis\Documents\nbatestjson.json") as f:
+  data = json.load(f)
+
+def teamDictionaryGenerator():
+    for team in data_leagueTeams:
+        leagueTeams[team["id"]] = team
+    for member in data_leagueMembers:
+        members[member["id"]] = member
+
+def findMatchup ():
+    my_date = datetime.today()
+    year, week_num, day_of_week = my_date.isocalendar()
+    lS_year, lS_week_num, lS_day_of_week = leagueBeginning.isocalendar()
+    if week_num < 52: 
+        week_num += 53
+    leagueWeek = week_num - lS_week_num +1 #positive number means 2021
+    for matchup in leagueSchedule:
+        tempUse = {}
+        if matchup["away"]["teamId"] == teamNumber:
+            opponentTeamId = int(matchup["home"]["teamId"])
+            tempUse["teamId"] = opponentTeamId
+            tempUse["matchupWeek"] = int(matchup["matchupPeriodId"]) - leagueWeek
+            tempUse["opponentName"] = members[leagueTeams[opponentTeamId]["owners"][0]]["firstName"] +" "+ members[leagueTeams[opponentTeamId]["owners"][0]]["lastName"]
+            tempUse["opponentTeam"] = leagueTeams[opponentTeamId]["nickname"]
+            mySchedule.append(tempUse)
+            print(tempUse)
+        elif matchup["home"]["teamId"] == teamNumber:
+            opponentTeamId = int(matchup["away"]["teamId"])
+            tempUse["teamId"] = opponentTeamId
+            tempUse["matchupWeek"] = int(matchup["matchupPeriodId"]) - leagueWeek
+            tempUse["opponentName"] = members[leagueTeams[opponentTeamId]["owners"][0]]["firstName"] +" "+ members[leagueTeams[opponentTeamId]["owners"][0]]["lastName"]
+            tempUse["opponentTeam"] = leagueTeams[opponentTeamId]["nickname"]
+            mySchedule.append(tempUse)
+            print(tempUse)
+
+def parseInput(inputVal):
+    if inputVal == 1:
+        findMatchup()
+        print("done collecting schedule...\n")
+    elif inputVal == 2:
+        print("computing H2H matchup")
+    elif inputVal == 3:
+        print("computing Trade matchup")
+
+# Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
+# print(data["draftDetail"])
+
+leagueSchedule = data["schedule"]
+data_leagueTeams = data["teams"]
+data_leagueMembers = data["members"]
+leagueTeams = {}
+members = {}
+mySchedule = []
+teamDictionaryGenerator()
+leagueBeginning = datetime.strptime('22 Dec 2020', '%d %b %Y')
+teamNumber = int(input (" Input team number: "))
+test_text = int(input ("\n Find Opponent        = 1\n Run H2H Analysis     = 2\n Trade Evaluation     = 3\n Waiver Wire Analysis = 4\n\n"))
+
+parseInput(test_text)
